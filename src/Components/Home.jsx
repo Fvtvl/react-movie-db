fabf1e0ddc986a4274a55d93d7cf5a091d54015c
Fvtvl <fvtvlnight@gmail.com>
2022-12-24 17:26:28 +0200

    create custom hook, image-bg, grid

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import API from '../API';
+import React from 'react';
+
+//Components
+import HeroImage from './HeroImage/HeroImage';
+
+//Hook
+import { useHomeFetch } from '../Hooks/useHomeFetch';
 
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from '../config';
 
 //Image 
 import NoImage from '../images/no_image.jpg'
+import Grid from './Grid/Grid';
+
 
 const Home = () => {
-
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
-
-    const fetchMovies = async (page, searchTerm = '') => {
-        try{
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm, page);
-            console.log(movies);
-
-        } catch (error) {
-            setError(true);
-
-        }
-    }
-
-    useEffect(()=>{
-        fetchMovies(1)
-    },[])
+    
+    const { state, loading, error } = useHomeFetch();
+    console.log(state);
 
   return (
-    <div>Home</div>
+    <>
+      {state.results[0] ?
+      <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}
+      /> : null
+      }
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </Grid>
+    </>
   )
 }
 
