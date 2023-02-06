2d6d69c28a37b74c3f3fbac0497a25fc8698ba10
Fvtvl <fvtvlnight@gmail.com>
2023-02-06 22:38:18 +0200

    add actors

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 //Components
-import HeroImage from './HeroImage/HeroImage';
-import Grid from './Grid/Grid';
-import Thumb from './Thumb/Thumb';
-import SearchBar from './SearchBar/SearchBar';
-import Button from './Button/Button';
-import Spinner from './Spinner/Spinner';
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import SearchBar from './SearchBar';
+import Button from './Button';
+import Spinner from './Spinner';
 
 //Hook
 import { useHomeFetch } from '../Hooks/useHomeFetch';
@@ -14,56 +14,45 @@ import { useHomeFetch } from '../Hooks/useHomeFetch';
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
-//Image 
+//Image
 import NoImage from '../images/no_image.jpg';
 
-
-
-
-
 const Home = () => {
-    
-    const { state,
-            loading,
-            error, 
-            setSearchTerm, 
-            searchTerm, 
-            setIsLoadingMore 
-          } = useHomeFetch();
+  const { state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore } =
+    useHomeFetch();
 
-  if(error) return <div>Something went wrong...</div>
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <>
-      {!searchTerm && state.results[0] ?
-      <HeroImage 
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-        title={state.results[0].original_title}
-        text={state.results[0].overview}
-      /> : null
-      }
-      <SearchBar setSearchTerm={setSearchTerm}/>
+      {!searchTerm && state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <SearchBar setSearchTerm={setSearchTerm} />
       <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
-        {state.results.map(movie => (
-          <Thumb 
+        {state.results.map((movie) => (
+          <Thumb
             key={movie.id}
             clickable
             movieId={movie.id}
             image={
               movie.poster_path
-              ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
-              : NoImage
-              }
-
-            />
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+          />
         ))}
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-        <Button text='Load More' callback={()=> setIsLoadingMore(true)}/>
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
-  )
-}
+  );
+};
 
 export default Home;
